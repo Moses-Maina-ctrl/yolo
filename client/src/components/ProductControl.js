18f87d7acfe7321a4296986de0df1d8fd1a2e5aa
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-06-03 19:17:02 +0300

    Change backend url from localhost

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -63,166 +63,168 @@ import EditProductForm from './EditProductForm';
 //         id: '6'
 //     }
 // ]
+backendUrl = '34.135.64.213'
 class ProductControl extends Component {
-    
-    constructor(props)  {
-        super(props);
-        this.state = {
-            formVisibleOnPage: false,
-            actualProductList: [],
-            selectedProduct: null,
-            editProduct: false,
-            uploadPhoto: null
-            
-        };
-    }
-    
-    componentDidMount(){
-        axios.get('http://localhost:5000/api/products')
-            .then(res =>{
-                console.log(res)
-                this.setState({
-                    actualProductList: res.data
-                })
-            })
-    }
-    handleEditProductClick = () =>{
-        console.log('HandleEditClick reached!!')
-        console.log(this.state.selectedProduct)
-        this.setState({
-            editProduct: true
-        })
-    }
-    handleAddButtonClick = (id) =>{
-        const BuyProduct = this.state.actualProductList.filter(product => product._id === id)[0];
-        BuyProduct.quantity = BuyProduct.quantity - 1;
-        if (BuyProduct.quantity <= 0) {
-            BuyProduct.quantity = "Product is not Available"
-        }
-        this.setState({
-            selectedProduct: BuyProduct
-        })
-    }
 
-    handleClick = () => {
-        if(this.state.editProduct){
-            this.setState({
-                editProduct: false
-            })
-        }else if (this.state.selectedProduct != null){
-            this.setState({
-                formVisibleOnPage: false,
-                selectedProduct: null
-            });
-        }else {
-            this.setState(prevState => ({
-                formVisibleOnPage: !prevState.formVisibleOnPage
-            }));
-        }
-    }
-    // handlePhotoUpload = (photo)=>{
-    //     console.log(photo.file)
-    //     this.setState({
-    //         uploadPhoto: file.file
-    //     })
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      actualProductList: [],
+      selectedProduct: null,
+      editProduct: false,
+      uploadPhoto: null
 
-        
-    // }
-
-    // Method to handle adding a new product
-    handleAddingNewProduct = (newProduct) =>{
-        // if (newProduct.photo === undefined){
-        //     newProduct.photo = Default_image
-        // }
-        // console.log(newProduct.name)
-        // const newProductList = this.state.actualProductList.concat(newProduct)
-        // var formData = new FormData()
-
-        // formData.append('data',newProduct)
-        // for (let key of Object.keys(newProduct)){
-        //     formData.set(key, newProduct[key])
-        // }
-        // formData.append('myFile', this.state.uploadPhoto)
-
-        // console.log(formData)   
-        // var formData = new FormData();
-        // formData.append('myImage',newProduct);
-        // const config = {
-        //     headers: {
-        //         'content-type': 'multipart/form-data'
-        //     }
-        // };
-        // console.log(formData) 
-        // for (var pair of formData.entries()) {
-        //     console.log(pair[0]+ ', ' + pair[1]); 
-        // }       
-        // console.log(...formData)
-        axios.post('http://localhost:5000/api/products', newProduct)
-            .then(res => console.log(res.data))
-        this.setState({
-            formVisibleOnPage: false
-        })
     };
-    handleDeletingProduct = (id) =>{
-        axios.delete('http://localhost:5000/api/products/'+id)
-            .then(res => console.log(res.data))
-            .catch((error) =>{
-                console.log(error)
-            })
-            this.setState({
-                actualProductList: this.state.actualProductList.filter(product => product._id !== id),
-                formVisibleOnPage: false,
-                selectedProduct: null
-            })
-    }
-    
-    // Method to handle click event on a product
-    handleChangingSelectedProduct = (id) => {
-        console.log(id)
-        const selectedProduct = this.state.actualProductList.filter(product => product._id === id)[0];
-        this.setState({selectedProduct: selectedProduct});
-    }
-    handleEditingProduct = (editedProduct) =>{
+  }
 
-        axios.put('http://localhost:5000/api/products/' + this.state.selectedProduct._id, editedProduct)
-            .then(res =>console.log(res.data))
-        
+  componentDidMount() {
+    axios.get(`${backendUrl}/api/products`)
+      .then(res => {
+        console.log(res)
         this.setState({
-            editProduct: false,
-            formVisibleOnPage: false
+          actualProductList: res.data
         })
-        window.location = '/';
+      })
+  }
+  handleEditProductClick = () => {
+    console.log('HandleEditClick reached!!')
+    console.log(this.state.selectedProduct)
+    this.setState({
+      editProduct: true
+    })
+  }
+  handleAddButtonClick = (id) => {
+    const BuyProduct = this.state.actualProductList.filter(product => product._id === id)[0];
+    BuyProduct.quantity = BuyProduct.quantity - 1;
+    if (BuyProduct.quantity <= 0) {
+      BuyProduct.quantity = "Product is not Available"
     }
+    this.setState({
+      selectedProduct: BuyProduct
+    })
+  }
 
-    render() {
-        let currentlyVisibleState = null;
-        let buttonText = null;
-        // let addProductButton = null;
-        if(this.state.editProduct){
-            currentlyVisibleState = <EditProductForm  product ={this.state.selectedProduct} onEditProduct = {this.handleEditingProduct} />
-            buttonText = "Back to Product Detail "
-        }else if (this.state.selectedProduct != null){
-            currentlyVisibleState = < ProductDetail product = {this.state.selectedProduct} onBuyButtonClick ={this.handleAddButtonClick}  onDeleteProduct = {this.handleDeletingProduct} onEditProductClick = {this.handleEditProductClick}/>
-            buttonText = "Back to product list"
-        }else if (this.state.formVisibleOnPage){
-            currentlyVisibleState = < NewProductForm onNewProductCreation = {this.handleAddingNewProduct} onPhotoUpload={this.handlePhotoUpload} />
-            buttonText = "Back to product list"
-        }else{
-            currentlyVisibleState = < ProductList productList = {this.state.actualProductList} onProductSelection={this.handleChangingSelectedProduct}  />
-            buttonText = "Add a product"
-            // addProductButton = <button onClick={this.handleClick} className="see-all-products text-center mx-auto">Add a product</button>
-        }
-        return (
-            <React.Fragment>
-                <AddProduct 
-                buttonText = {buttonText}
-                whenButtonClicked = {this.handleClick}
-                />
-                
-                {currentlyVisibleState}
-            </React.Fragment>
-        )
+  handleClick = () => {
+    if (this.state.editProduct) {
+      this.setState({
+        editProduct: false
+      })
+    } else if (this.state.selectedProduct != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedProduct: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
     }
+  }
+  // handlePhotoUpload = (photo)=>{
+  //     console.log(photo.file)
+  //     this.setState({
+  //         uploadPhoto: file.file
+  //     })
+
+
+  // }
+
+  // Method to handle adding a new product
+  handleAddingNewProduct = (newProduct) => {
+    // if (newProduct.photo === undefined){
+    //     newProduct.photo = Default_image
+    // }
+    // console.log(newProduct.name)
+    // const newProductList = this.state.actualProductList.concat(newProduct)
+    // var formData = new FormData()
+
+    // formData.append('data',newProduct)
+    // for (let key of Object.keys(newProduct)){
+    //     formData.set(key, newProduct[key])
+    // }
+    // formData.append('myFile', this.state.uploadPhoto)
+
+    // console.log(formData)   
+    // var formData = new FormData();
+    // formData.append('myImage',newProduct);
+    // const config = {
+    //     headers: {
+    //         'content-type': 'multipart/form-data'
+    //     }
+    // };
+    // console.log(formData) 
+    // for (var pair of formData.entries()) {
+    //     console.log(pair[0]+ ', ' + pair[1]); 
+    // }       
+    // console.log(...formData)
+    axios.post(`${backendUrl}/api/products`, newProduct)
+
+      .then(res => console.log(res.data))
+    this.setState({
+      formVisibleOnPage: false
+    })
+  };
+  handleDeletingProduct = (id) => {
+    axios.delete('34.28.238.85/api/products/' + id)
+      .then(res => console.log(res.data))
+      .catch((error) => {
+        console.log(error)
+      })
+    this.setState({
+      actualProductList: this.state.actualProductList.filter(product => product._id !== id),
+      formVisibleOnPage: false,
+      selectedProduct: null
+    })
+  }
+
+  // Method to handle click event on a product
+  handleChangingSelectedProduct = (id) => {
+    console.log(id)
+    const selectedProduct = this.state.actualProductList.filter(product => product._id === id)[0];
+    this.setState({ selectedProduct: selectedProduct });
+  }
+  handleEditingProduct = (editedProduct) => {
+
+    axios.put('34.28.238.85/api/products/' + this.state.selectedProduct._id, editedProduct)
+      .then(res => console.log(res.data))
+
+    this.setState({
+      editProduct: false,
+      formVisibleOnPage: false
+    })
+    window.location = '/';
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    // let addProductButton = null;
+    if (this.state.editProduct) {
+      currentlyVisibleState = <EditProductForm product={this.state.selectedProduct} onEditProduct={this.handleEditingProduct} />
+      buttonText = "Back to Product Detail "
+    } else if (this.state.selectedProduct != null) {
+      currentlyVisibleState = < ProductDetail product={this.state.selectedProduct} onBuyButtonClick={this.handleAddButtonClick} onDeleteProduct={this.handleDeletingProduct} onEditProductClick={this.handleEditProductClick} />
+      buttonText = "Back to product list"
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = < NewProductForm onNewProductCreation={this.handleAddingNewProduct} onPhotoUpload={this.handlePhotoUpload} />
+      buttonText = "Back to product list"
+    } else {
+      currentlyVisibleState = < ProductList productList={this.state.actualProductList} onProductSelection={this.handleChangingSelectedProduct} />
+      buttonText = "Add a product"
+      // addProductButton = <button onClick={this.handleClick} className="see-all-products text-center mx-auto">Add a product</button>
+    }
+    return (
+      <React.Fragment>
+        <AddProduct
+          buttonText={buttonText}
+          whenButtonClicked={this.handleClick}
+        />
+
+        {currentlyVisibleState}
+      </React.Fragment>
+    )
+  }
 }
 
 export default ProductControl;
